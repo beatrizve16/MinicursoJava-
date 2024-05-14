8699bebd63302fafd3fe545eb06639a045cd00ab
Beatriz Vasconcellos Espindola <165811496+beatrizve16@users.noreply.github.com>
2024-05-14 12:04:22 -0300

    Create classe.java

---
+++ Aula 9/Conta bancaria/classe.java
@@ -0,0 +1,25 @@
+
+
+package ContaBancaria.Entidades;
+
+public class ContaBancaria {
+
+    public double numero;
+    public double saldo;
+    public double sacar;
+    public double depositar;
+
+    public double funcDepositar(double saldo, double depositar){
+         saldo += depositar;
+         return saldo;
+    }
+
+    public double funcSacar(double saldo, double sacar){
+        saldo -= sacar;
+        return saldo; 
+    }
+
+    public double funcVerificar(double saldo){
+        return (saldo);
+    }
+}
