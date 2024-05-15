d3769c7cf1995a7bdc326de21ec42a7a9acd975c
Beatriz Vasconcellos Espindola <165811496+beatrizve16@users.noreply.github.com>
2024-05-15 16:32:05 -0300

    Create classeFiguraGeometrica.java

---
+++ teste/classeFiguraGeometrica.java
@@ -0,0 +1,15 @@
+public class FiguraGeometrica {
+    protected String nome;
+
+    public FiguraGeometrica(String nome) {
+        this.nome = nome;
+    }
+
+    public double calcularArea() {
+        return 0;
+    }
+
+    public double calcularPerimetro() {
+        return 0;
+    }
+}
