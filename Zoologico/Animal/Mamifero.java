85fe47cac0bf6c9f5dac2cd95b016d50c467fee1
Beatriz Vasconcellos Espindola <165811496+beatrizve16@users.noreply.github.com>
2024-05-16 12:27:38 -0300

    Create Mamifero.java

---
+++ Zoologico/Animal/Mamifero.java
@@ -0,0 +1,8 @@
+package Zoologico.Entidades;
+
+public class Mamifero extends Animal {
+    public String tipoPelo;
+    ]public void dormindo()
+    dormindo()
+    System.out.println("O mamifero esta dormindo");
+}
